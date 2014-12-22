198b36519ff42221c3170caf613379c861fa9613
Hiroshi Hatake <hatake@clear-code.com>
2014-12-22 13:43:29 +0900

    Untabify

--- content/messenger-overlay.js
+++ content/messenger-overlay.js
@@ -8,63 +8,63 @@
     let observerService = Cc["@mozilla.org/observer-service;1"].getService(Ci.nsIObserverService);
 
     document.addEventListener("DOMContentLoaded", function onDOMContentLoaded(aEvent) {
-	document.removeEventListener("DOMContentLoaded", onDOMContentLoaded);
-	var servers = collectAccountsToBeLoggedIn();
-	if (servers.length == 0)
-	    return;
+        document.removeEventListener("DOMContentLoaded", onDOMContentLoaded);
+        var servers = collectAccountsToBeLoggedIn();
+        if (servers.length == 0)
+            return;
 
-	let loadObserver = {
-	    observe : function onLoaded(aEvent) {
-		let msgWindow = MailServices.mailSession.topmostMsgWindow;
-		observerService.removeObserver(loadObserver, 'mail-startup-done', false);
+        let loadObserver = {
+            observe : function onLoaded(aEvent) {
+                let msgWindow = MailServices.mailSession.topmostMsgWindow;
+                observerService.removeObserver(loadObserver, 'mail-startup-done', false);
 
-		let listener = {
-		    successCount: 0,
-		    failureCount: 0,
-		    OnStartRunningUrl: function () {
-		    },
-		    OnStopRunningUrl: function (url, exitCode) {
-			if (Components.isSuccessCode(exitCode)) {
-			    this.successCount++;
-			} else {
-			    this.failureCount++;
-			}
-			this.checkFinish();
-		    },
+                let listener = {
+                    successCount: 0,
+                    failureCount: 0,
+                    OnStartRunningUrl: function () {
+                    },
+                    OnStopRunningUrl: function (url, exitCode) {
+                        if (Components.isSuccessCode(exitCode)) {
+                            this.successCount++;
+                        } else {
+                            this.failureCount++;
+                        }
+                        this.checkFinish();
+                    },
 
-		    checkFinish: function () {
-			if (this.successCount + this.failureCount != servers.length)
-			    return;
+                    checkFinish: function () {
+                        if (this.successCount + this.failureCount != servers.length)
+                            return;
 
-			if (this.successCount == servers.length) {
-			    document.documentElement.style.visibility = "";
-			} else {
-			    window.close();
-			}
-		    }
-		};
-		for (var i = 0, maxServer = servers.length; i < maxServer; ++i) {
-		    servers[i].verifyLogon(listener, msgWindow);
-		}
-	    }
-	};
+                        if (this.successCount == servers.length) {
+                            document.documentElement.style.visibility = "";
+                        } else {
+                            window.close();
+                        }
+                    }
+                };
+                for (var i = 0, maxServer = servers.length; i < maxServer; ++i) {
+                    servers[i].verifyLogon(listener, msgWindow);
+                }
+            }
+        };
 
-	observerService.addObserver(loadObserver, 'mail-startup-done', false);
+        observerService.addObserver(loadObserver, 'mail-startup-done', false);
 
-	document.documentElement.style.visibility = "hidden";
+        document.documentElement.style.visibility = "hidden";
     });
 
     function collectAccountsToBeLoggedIn() {
-	var accountManager = MailServices.accounts;
-	var allServers = accountManager.allServers;
-	var serversToBeLoggedIn = [];
-	for (var i = 0,  maxi = allServers.length; i < maxi; ++i) {
-	    let currentServer = allServers.queryElementAt(i, Ci.nsIMsgIncomingServer);
-	    if (currentServer.type == "none")
-		continue;
+        var accountManager = MailServices.accounts;
+        var allServers = accountManager.allServers;
+        var serversToBeLoggedIn = [];
+        for (var i = 0,  maxi = allServers.length; i < maxi; ++i) {
+            let currentServer = allServers.queryElementAt(i, Ci.nsIMsgIncomingServer);
+            if (currentServer.type == "none")
+                continue;
 
-	    serversToBeLoggedIn.push(currentServer);
-	}
-	return serversToBeLoggedIn;
+            serversToBeLoggedIn.push(currentServer);
+        }
+        return serversToBeLoggedIn;
     }
 })();
